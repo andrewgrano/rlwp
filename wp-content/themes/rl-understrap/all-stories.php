905e2496098ef2450d30dff19ac7d3be727b5917
andrewgrano <andrewgrano@gmail.com>
2020-01-26 15:16:02 -0800

    all stories testing

--- wp-content/themes/rl-understrap/all-stories.php
+++ wp-content/themes/rl-understrap/all-stories.php
@@ -25,17 +25,18 @@ $sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
                 <div class="col-lg-9 col-md-11">
                     <h1 class="pageHeading">All Stories.</h1>
 
-                    <?php if ( have_posts() ) : ?>
-                        <?php
-                            global $query_string;
-                            query_posts( $query_string . '&posts_per_page=12' );
-                         ?>
+                    <?php
+                    $args = array(
+                        'posts_per_page' => '12',
+                    );
+
+                    $query = new WP_query ( $args );
+                    if ( $query->have_posts() ) { ?>
+
 
                         <div class="row justify-content-md-center justify-content-lg-start">
 
-                            <?php /* Start the Loop */ ?>
-
-                            <?php while ( have_posts() ) : the_post(); ?>
+                             <?php while ( $query->have_posts() ) : $query->the_post(); /* start the loop */ ?>
                                 <div class="col-lg-4 col-md-6">
 
                                     <?php
